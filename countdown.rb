60ec2af4b324bf83fdcf76c57c592f2e3bbbfd7b
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-08-23 16:45:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,5 @@
 #write your code here
 
 def countdown(num)
-  num = 10
-   while num > 0
-    num = num-1
- 
-     puts "#{num} SECOND(S)!"
-  
-  
-  end
-    puts "HAPPY NEW YEAR!"
-  
-end
+  num = log 
+  while 
